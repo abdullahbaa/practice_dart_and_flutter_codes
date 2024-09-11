ab85cfc5476cfe3463d3c4bda0f13849a23092ca
abdullahraihan03 <aaziz191231@bscse.uiu.ac.bd>
2024-09-11 16:34:45 +0600

    first commit

---
+++ ios/lib/practice.dart
@@ -0,0 +1,40 @@
+
+import 'dart:io';
+
+void main(){
+  print('welcome to Dart!');
+
+  stdout.write('enter your name:'); //Print the data
+  var name = stdin.readLineSync(); //Input the data
+
+  print("Welcome,$name");
+
+//creating an class object
+  Human();
+
+  //Declaration of Variable
+   int a;
+//Assign
+ a=5; //initialization
+
+  BigInt longValue;
+  longValue = BigInt.parse("2705620349850692347654934605");
+  /*print(a);
+*//*a=7;*/
+  print(longValue);
+
+  num percentage =99.65; //num variable percentages and fraction values insert kora jay.
+  bool islogin = false;
+  islogin=true;
+
+  //inline declaration
+
+/*String name = "Rahim";*/
+
+}
+
+class Human{
+
+  Human();
+
+}
